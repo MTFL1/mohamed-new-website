bf3ec677a2b70990bc929682d1f0734fae430606
MTFL1 <m.ecomplus1@gmail.com>
2025-09-06 13:09:01 +0200

    fix meta icon

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import "./App.css";
 
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+      <Analytics />
     </div>
   );
 }
